268f48cc15bc9c7d48f0dd8b32458d976098870d
Derlie Monne <derlie.monne@gmail.com>
2023-07-07 14:18:51 +1000

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ClassicCalculator
+{
+    public enum Digit
+    {
+        Zero,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+    }
+
+    public enum BinaryAction
+    {
+        None,
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+    }
+
+    class Calculator
+    {
+        public const string ZERO = "0";
+
+        public static Dictionary<BinaryAction, string> BinaryActionDisplay = new()
+        {
+            { BinaryAction.Add, "+" },
+            { BinaryAction.Subtract, "-" },
+            { BinaryAction.Multiply, "*" },
+            { BinaryAction.Divide, "/" },
+        };
+
+        private double? _bufferNumber;
+        private string _activeNumber;
+        private BinaryAction _binaryAction;
+        private int _digitsCount = 16;
+
+        public int DigitsCount
+        {
+            get => _digitsCount;
+            set => _digitsCount = value;
+        }
+
+        public Calculator()
+        {
+            _binaryAction = BinaryAction.None;
+            _activeNumber = ZERO;
+        }
+
+        public string GetUpperDisplayText()
+        {
+            string output = _bufferNumber?.ToString() ?? "";
+            if (BinaryActionDisplay.TryGetValue(_binaryAction, out string? binaryActionDisplay) &&
+                binaryActionDisplay is not null)
+            {
+                output += " " + binaryActionDisplay;
+            }
+            return output;
+        }
+
+        public string GetBottomDisplayText()
+        {
+            return _activeNumber;
+
+        }
+
+        public void Negate()
+        {
+            SetActiveNumberSafe(-ParseSafe(_activeNumber));
+        }
+
+        public void Clear()
+        {
+            _bufferNumber = null;
+            _activeNumber = ZERO;
+            _binaryAction = BinaryAction.None;
+        }
+
+        public void ClearEntry()
+        {
+            _activeNumber = ZERO;
+        }
+
+        public void Backspace()
+        {
+            if (_activeNumber is null)
+            {
+                return;
+            }
+
+            string newNumber = _activeNumber.Substring(0, _activeNumber.Length - 1);
+            if (newNumber.Length == 0)
+            {
+                _activeNumber = ZERO;
+            }
+            else
+            {
+                SetActiveNumberSafe(ParseSafe(newNumber));
+            }
+        }
+
+        public void Percent()
+        {
+            SetActiveNumberSafe(ParseSafe(_activeNumber) / 100.0);
+        }
+
+        public void Sqrt()
+        {
+            if (_activeNumber is null || _activeNumber == "") return;
+            SetActiveNumberSafe(double.Sqrt(ParseSafe(_activeNumber)));
+        }
+
+        public void Divide()
+        {
+            _bufferNumber = ParseSafe(_activeNumber);
+            _activeNumber = ZERO;
+            _binaryAction = BinaryAction.Divide;
+        }
+
+        public void Multiply()
+        {
+            _bufferNumber = ParseSafe(_activeNumber);
+            _activeNumber = ZERO;
+            _binaryAction = BinaryAction.Multiply;
+
+        }
+
+        public void Subtract()
+        {
+            _bufferNumber = ParseSafe(_activeNumber);
+            _activeNumber = ZERO;
+            _binaryAction = BinaryAction.Subtract;
+        }
+
+        public void Add()
+        {
+            _bufferNumber = ParseSafe(_activeNumber);
+            _activeNumber = ZERO;
+            _binaryAction = BinaryAction.Add;
+        }
+
+        public void Reciprocate()
+        {
+            SetActiveNumberSafe(1 / ParseSafe(_activeNumber));
+        }
+
+        public void Digit(Digit digit)
+        {
+            _activeNumber ??= ZERO;
+
+            string concatenated = _activeNumber + ((int)digit).ToString();
+            SetActiveNumberSafe(ParseSafe(concatenated));
+        }
+
+        public void DecimalPoint()
+        {
+            if (_activeNumber.Contains(".") || _activeNumber.Contains(","))
+            {
+                return;
+            }
+
+            _activeNumber ??= ZERO;
+            _activeNumber += ".";
+        }
+
+        public void Calculate()
+        {
+            double? result;
+            double activeNumber = ParseSafe(_activeNumber);
+            if (_binaryAction == BinaryAction.None)
+            {
+                return;
+            }
+            else if (_binaryAction == BinaryAction.Add)
+            {
+                result = _bufferNumber + activeNumber;
+            }
+            else if (_binaryAction == BinaryAction.Subtract)
+            {
+                result = _bufferNumber - activeNumber;
+            }
+            else if (_binaryAction == BinaryAction.Multiply)
+            {
+                result = _bufferNumber * activeNumber;
+            }
+            else if (_binaryAction == BinaryAction.Divide)
+            {
+                result = _bufferNumber / activeNumber;
+            }
+            else
+            {
+                throw new ArgumentException("BinaryAction does not handle all its possible values.");
+            }
+
+            if (result is null)
+            {
+                throw new Exception("Internal Error. Result is null.");
+            }
+            else
+            {
+                SetActiveNumberSafe(result.Value);
+            }
+            _bufferNumber = null;
+            _binaryAction = BinaryAction.None;
+        }
+
+        private double ParseSafe(string number)
+        {
+            if (double.TryParse(number, CultureInfo.InvariantCulture, out double parsed))
+            {
+                return parsed;
+            }
+            else
+            {
+#if (DEBUG)
+                // Should never execute.
+                throw new FormatException("Internal error.");
+#else
+                return 0;
+#endif
+            }
+        }
+
+        private void SetActiveNumberSafe(double number)
+        {
+            _activeNumber = number.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace ClassicCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Calculator _calculator;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _calculator = new Calculator();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            UpperDisplay.Content = _calculator.GetUpperDisplayText();
+            BottomDisplay.Content = _calculator.GetBottomDisplayText();
+        }
+
+        private void Backspace_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Backspace();
+        }
+
+        private void ClearEntry_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.ClearEntry();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Clear();
+        }
+
+        private void Negate_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Negate();
+        }
+
+        private void Sqrt_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Sqrt();
+        }
+
+        private void Seven_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Digit(Digit.Seven);
+        }
+
+        private void Eight_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Digit(Digit.Eight);
+        }
+
+        private void Nine_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Digit(Digit.Nine);
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Divide();
+        }
+
+        private void Percent_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Percent();
+        }
+
+        private void Four_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Digit(Digit.Four);
+        }
+
+        private void Five_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Digit(Digit.Five);
+        }
+
+        private void Six_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Digit(Digit.Six);
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Multiply();
+        }
+
+        private void Reciprocate_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Reciprocate();
+        }
+
+        private void One_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Digit(Digit.One);
+        }
+
+        private void Two_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Digit(Digit.Two);
+        }
+
+        private void Three_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Digit(Digit.Three);
+        }
+
+        private void Subtract_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Subtract();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Calculate();
+        }
+
+        private void Zero_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Digit(Digit.Zero);
+        }
+
+        private void DecimalPoint_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.DecimalPoint();
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            _calculator.Add();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                _calculator.Calculate();
+            }
+            else if (e.Key == Key.OemPlus)
+            {
+                _calculator.Add();
+            }
+            else if (e.Key == Key.OemMinus)
+            {
+                _calculator.Subtract();
+            }
+            else if (e.Key == Key.OemQuestion)
+            {
+                _calculator.Divide();
+            }
+            else if (e.Key == Key.D8 && Keyboard.Modifiers == ModifierKeys.Shift)
+            {
+                _calculator.Multiply();
+            }
+            else if (e.Key == Key.Back)
+            {
+                _calculator.Backspace();
+            }
+            else if (e.Key == Key.D0)
+            {
+                _calculator.Digit(Digit.Zero);
+            }
+            else if (e.Key == Key.D1)
+            {
+                _calculator.Digit(Digit.One);
+            }
+            else if (e.Key == Key.D2)
+            {
+                _calculator.Digit(Digit.Two);
+            }
+            else if (e.Key == Key.D3)
+            {
+                _calculator.Digit(Digit.Three);
+            }
+            else if (e.Key == Key.D4)
+            {
+                _calculator.Digit(Digit.Four);
+            }
+            else if (e.Key == Key.D5)
+            {
+                _calculator.Digit(Digit.Five);
+            }
+            else if (e.Key == Key.D6)
+            {
+                _calculator.Digit(Digit.Six);
+            }
+            else if (e.Key == Key.D7)
+            {
+                _calculator.Digit(Digit.Seven);
+            }
+            else if (e.Key == Key.D8)
+            {
+                _calculator.Digit(Digit.Eight);
+            }
+            else if (e.Key == Key.D9)
+            {
+                _calculator.Digit(Digit.Nine);
+            }
+            else if (e.Key == Key.OemComma)
+            {
+                _calculator.DecimalPoint();
+            }
+            e.Handled = true;
+        }
+
+        private void Window_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (e.Text == "*")
+            {
+                _calculator.Multiply();
+            }
+            //if (e.Text == "," || e.Text == ".")
+            //{
+            //    _calculator.DecimalPoint();
+            //}
+        }
+    }
+}
